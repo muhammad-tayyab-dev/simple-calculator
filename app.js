6c8f1e3c3871a804db36540451edd576682338ab
muhammad-tayyab-dev <126325265+muhammad-tayyab-dev@users.noreply.github.com>
2023-05-20 19:23:30 +0500

    add js

---
+++ app.js
@@ -0,0 +1,13 @@
+var result = document.getElementById("result").value
+function get_num(num) {
+document.getElementById("result").value +=num
+}
+function clearResult() {
+  document.getElementById("result").value = ""
+}
+function getResult(){
+  document.getElementById("result").value = eval(document.getElementById("result").value)
+}
+function clear_num(){
+  document.getElementById('result').value = document.getElementById('result').value.slice(0,result.length - 1);
+}
